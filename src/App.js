cc5619dcf2c83fe46a926dcc688280780e09776c
Samuel Cicon <sam@cicon.org>
2021-08-12 14:37:40 -0600

    Updated the website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState} from "react";
+
 function App() {
+  const [number, setNumber] = useState(1);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO WORLD!
         </p>
+
+        <p>
+          This is Sam's site, we will one day rule the world!
+        </p>
+
+        <div className="button" onClick={() => setNumber((number + 1))}>
+          INCREASE GROFIT
+        </div>
+
+        <div>
+          <code>{number}</code>
+        </div>
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload
+        </p>
+
+
         <a
           className="App-link"
-          href="https://semlar.com/"
+          href="https://www.churchofjesuschrist.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
